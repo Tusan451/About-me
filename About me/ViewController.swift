ef2228aa400bab4863eace5e65f50aaaa3ec778a
Oleg Fedotov <trevor706@gmail.com>
2022-08-09 20:22:15 +0300

    Launch screen UI

--- About me/ViewController.swift
+++ About me/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet var startButton: UIButton!
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view.
+        startButton.layer.cornerRadius = 12
     }
 
 
